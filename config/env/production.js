473b5a620fe5b71e18be9bc4ec5ab67388b55e2c
aya-shahal <ayashahal.inventus@gmail.com>
2025-01-16 22:39:38 +0200

    fixed

--- config/env/production.js
+++ config/env/production.js
@@ -3,7 +3,7 @@ module.exports = {
          url: 'mongodb+srv://ayashahal055:f1vNyuMbHprQacGj@cluster0.2tqag.mongodb.net/'
      },
     app:{
-        imageurl : "https://apix.piu-paris.com/upload",
+        imageurl : "https://uniright-api.onrender.com/upload",
         url: 'http://188.166.52.152:3003',
         localpathx:"/root/uniright-api/public/upload/"
     }
